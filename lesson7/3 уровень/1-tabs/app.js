5dfa3158e4fcf4a31890214f65ef9bf827003b7e
Oksana Dubik <oksana170773@gmail.com>
2021-07-14 22:05:12 +0600

    7 урок закончила 2 уровень

--- lesson7/3 уровень/1-tabs/app.js
+++ lesson7/3 уровень/1-tabs/app.js
@@ -11,33 +11,52 @@ const texts = {
     2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
     обработчик клика функцию clickHandler.
 */
+let linkText = document.querySelector("const");
+let navLinksList = document.getElementsByClassName("nav-link");
 
 
 /**
  * Обработчик клика по .nav-link
- * @param {MouseEvent} event 
+ * @param {MouseEvent} event
  */
 function clickHandler(event) {
     // здесь вызывайте changeText и changeActiveClass, и передавайте
     // им объект события.
-   
+
+
+    changeText(event);
+    changeActiveClass(event);
 }
 
 /**
  * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
  * на тот, по которому кликнули.
- * @param {MouseEvent} event 
+ * @param {MouseEvent} event
  */
 function changeActiveClass(event) {
-    
+    for (let i = 0; i < navLinksList.length; i++) {
+
+            if (!navLinksList[i].classList.contains("active")) {
+
+                navLinksList[i].classList.add("active");
+                navLinksList[i - 1].classList.remove("active");
+            }
+
+    }
 }
 
 /**
- * Эта фукнция должна читать текст (например через textContent) из 
+ * Эта фукнция должна читать текст (например через textContent) из
  * .nav-link по которому кликнули и в зависимости от этого в .text
  * ставить соответствующий текст из texts.
- * @param {MouseEvent} event 
+ * @param {MouseEvent} event
  */
 function changeText(event) {
-    
+    for (let i = 0; i < navLinksList.length; i++) {
+       let text= navLinksList[i].textContent;
+console.log(text);
+
+    }
+
+
 }
